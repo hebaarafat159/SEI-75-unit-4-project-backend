a69814e100719261e1ede63957517551214254ca
hebaarafat159 <heba.arafat159@gmail.com>
2023-11-23 15:01:40 +0000

    fix reagistration issue

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,11 +1,23 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Book, Author, Photo, Request, Customer
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'id']
+        fields = ['username', 'email', 'id', 'password']
+    
+    def validate_password(self, value: str) -> str:
+        """
+        Hash value passed by user.
+
+        :param value: password of a user
+        :return: a hashed version of the password
+        """
+        if not value:
+            return value
+        return make_password(value)
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
